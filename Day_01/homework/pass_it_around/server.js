1030b2681c6d697b05f0a5fe5f81b7ae0f4e238d
John Torres <rebuke.devour@gmail.com>
2021-10-19 00:04:18 -0400

    Take one down

---
+++ Day_01/homework/pass_it_around/server.js
@@ -0,0 +1,15 @@
+const express require('express');
+const app = express();
+
+app.get('/:bottlesNum',('req,res')=> {
+    console.log('99 bottles of beer on the wall <a href = '/98)
+    if()
+})
+
+app.get("/", (req, res) => {
+    res.send(`99 bottles of acid on the wall <a href="/98">take one down, pass it around</a>`)
+})
+
+app.listen(3000, ()=> {
+    console.log('Your server is now listening on port 3000')
+})
